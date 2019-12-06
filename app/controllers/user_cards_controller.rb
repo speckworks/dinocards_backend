c6cfa6c948295fbb57e26d7787f21f8155f755eb
John Speck <speckworks@gmail.com>
2019-12-05 23:31:56 -0500

    mvp+CRD

--- app/controllers/user_cards_controller.rb
+++ app/controllers/user_cards_controller.rb
@@ -1,7 +1,24 @@
 class UserCardsController < ApplicationController
       def index
         @usercards = UserCard.all
-        render json: @usercards
+        @colors = Color.all
+        @dinos = Dino.all
+        @users = User.all
+
+        @dinocards = []
+
+        @usercards.map do |card|
+          dinocard = {}
+          dinocard[:id] = card.id
+          colorObj = @colors.find {|color| color.id == card.color_id}
+          dinocard[:color] = colorObj.color
+          dinoObj = @dinos.find {|dino| dino.id ==  card.dino_id}
+          dinocard[:img] = dinoObj.img
+          userObj = @users.find {|user| user.id == card.user_id}
+          dinocard[:name]= userObj.name
+          @dinocards<<dinocard
+        end
+        render json: @dinocards
       end
       
       def show
